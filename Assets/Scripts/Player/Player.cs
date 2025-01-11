048f448779876459ad82c8aaa4bffc385ee92bdc
Thiago de Araújo <araujod.thiago@gmail.com>
2025-01-11 13:40:26 -0300

    Add Jumpo to Prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,10 +7,10 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;
     public Vector2 velocity;
     public float speed;
-
+    public float forcejump = 2;
 
     
-
+    
     void Start()
     {
         
@@ -18,13 +18,29 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) 
-        { 
-            rb.velocity = new Vector2 (-speed, rb.velocity.y);
+        HandleMoviments();
+        HandleJump();
+    }
+
+    private void HandleMoviments()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+    }
+
+    private void HandleJump()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * forcejump;
         }
     }
 }
