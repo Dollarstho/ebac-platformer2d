b04d76be24ebace091829eadcb00f07f40700e60
Thiago de Araújo <araujod.thiago@gmail.com>
2025-01-12 18:37:39 -0300

    Implement Jump Animation System

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Player : MonoBehaviour
 {
+    [Header("Moviment setup")]
+    public BoxCollider2D BoxCollider2D;
     public Rigidbody2D rb;
     public Vector2 velocity;
     public float speed;
     public float forcejump = 2;
     public float speedRun;
 
+    [Header("Animation setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = .7f;
+    public float animationDuration = .1f;
+    public Ease ease = Ease.OutBack;
+
     private float _currentspeed;
+
+    private bool _isJump = false;
 
     
     
@@ -52,6 +62,22 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = Vector2.up * forcejump;
+            rb.transform.localScale = Vector2.one;
+
+            DOTween.Kill(rb.transform);
+
+            HandleScaleJump();
+
+            _isJump = true;
+            
         }
+    }
+
+    private void HandleScaleJump()
+    {
+        rb.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        rb.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+
+        
     }
 }
